c3f3471fb4aa0d7b0b249ace32fbf9e891ba1972
at-oanhlt <oanhlt@asiantech.vn>
2017-07-07 15:40:52 +0700

    using component

--- App.jsx
+++ App.jsx
@@ -1,22 +1,90 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: [
+        {
+          "id":1,
+          "name":"Foo",
+          "age":"20"
+        },
+
+        {
+          "id":2,
+          "name":"Bar",
+          "age":"30"
+        },
+
+        {
+          "id":3,
+          "name":"Baz",
+          "age":"40"
+        }
+      ]
+    }
+  }
+
   render() {
     var i = 0;
     var myStyle = {
       fontSize: '200',
       color: 'red'
     }
+
     return (
       <div>
         <h1 style= {myStyle}>{i == 1 ? 'True!' : 'False!' }</h1>
-        <h4>Header</h4>
-        <h2>Content</h2>
+        <h4>Header Static</h4>
+        <h2>Content Static</h2>
         <p data-myattribte="somevalue">This is the content</p>
         <h3>{1+1}</h3>
+
+
+        <Header/>
+        <table>
+          <tbody>
+            {this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
+          </tbody>
+        </table>
+        <Content/>
       </div>
     );
   }
 }
 
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Header</h1>
+      </div>
+    )
+  }
+}
+
+class Content extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Content</h2>
+        <p>The content text!!!</p>
+      </div>
+    )
+  }
+}
+
+class TableRow extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.data.id}</td>
+        <td>{this.props.data.name}</td>
+        <td>{this.props.data.age}</td>
+      </tr>
+    )
+  }
+}
 export default App;
